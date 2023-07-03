e86130bd5da5ddb45ca8a0ef516999fa5188d083
Alice (SuryaninovSA) <small046@ya.ru>
2023-07-03 18:56:24 +0300

    code: добавил вывод максимума

--- main.cpp
+++ main.cpp
@@ -9,5 +9,12 @@ int main()
     cin >> a >> b;
     a / b;
     a % b;
-    cout << a + b;
+    cout << "A + B = " << a + b << '\n'
+    << "A - B = " << a - b << '\n'
+    << "A * B = " << a * b << '\n'
+    << "A / B = " << a / b << '\n';
+    if (a < b)
+        cout << "max: "<< b;
+    if (b < a)
+        cout << "max: "<< a;
 }
