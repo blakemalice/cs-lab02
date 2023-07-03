762cffc6dea4ff7f6e65b44d7b2d0ff9e8fdcf2c
Bob (Bob) <bob@example.com>
2023-07-03 19:39:32 +0300

    code: добавил вывод минимума

--- main.cpp
+++ main.cpp
@@ -9,5 +9,17 @@ int main()
     cin >> a >> b;
     a / b;
     a % b;
-    cout << a + b;
+    cout << "A + B = " << a + b << '\n'
+    << "A - B = " << a - b << '\n'
+    << "A * B = " << a * b << '\n'
+    << "A / B = " << a / b << '\n';
+    if (a < b)
+        cout << "max: "<< b;
+    if (b < a)
+        cout << "max: "<< a;
+    if (a < b)
+        cout << "min: "<< a;
+    if (b < a)
+        cout << "min: "<< b;
+
 }
